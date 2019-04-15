3664a45ab2bc8307528317a75adc539276c2b71d
Adam Crow <adamcrow63@gmail.com>
2019-04-15 15:12:43 +1000

    Updated to v2.3.0

--- src/main/java/life/genny/bridge/ServiceVerticle.java
+++ src/main/java/life/genny/bridge/ServiceVerticle.java
@@ -1,30 +1,44 @@
 package life.genny.bridge;
 
 
+import java.lang.invoke.MethodHandles;
+
+import org.apache.logging.log4j.Logger;
+
 import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.core.Future;
 import life.genny.channel.Routers;
 import life.genny.cluster.Cluster;
+import life.genny.eventbus.EventBusInterface;
+import life.genny.eventbus.EventBusVertx;
+import life.genny.eventbus.VertxCache;
+import life.genny.qwandautils.GennyCacheInterface;
 import life.genny.security.SecureResources;
+import life.genny.utils.VertxUtils;
 
 public class ServiceVerticle extends AbstractVerticle {
 
+	  protected static final Logger log = org.apache.logging.log4j.LogManager
+		      .getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+
+
   @Override
   public void start() {
-    System.out.println("Setting up routes");
+    log.info("Setting up routes");
     final Future<Void> startFuture = Future.future();
     Cluster.joinCluster().compose(res -> {
-      final Future<Void> fut = Future.future();
-      SecureResources.setKeycloakJsonMap().compose(p -> {
-    	Routers.routers(vertx);
-        BridgeRouters.routers(vertx);
-        Routers.activate(vertx);
-        System.out.println("Bridge now ready");
-        fut.complete();
-      }, fut);
+      EventBusInterface eventBus = new EventBusVertx();
+      GennyCacheInterface vertxCache = new VertxCache();
+      VertxUtils.init(eventBus,vertxCache);
+      Routers.routers(vertx);
+      BridgeRouters.routers(vertx);
+      Routers.activate(vertx);
+      log.info("Pontoon now ready");
+      
       EBCHandlers.registerHandlers();
       startFuture.complete();
     }, startFuture);
     
   }
+ 
 }
